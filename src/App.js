64e8e09ab2802dcb2130d092b5e8b01eb53d7969
XandyGomes <x3.computadores@hotmail.com>
2022-03-19 12:38:50 -0300

    aula

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 import Intervalo from './components/Intervalo'
@@ -6,20 +6,21 @@ import Media from './components/Media'
 import Soma from './components/Soma'
 import Sorteio from './components/Sorteio'
 
-
 function App() {
+  const [min, setMin] = useState(10)
+  const [max, setMax] = useState(100)
   return (
     <div className='App'>
       <h1>Projeto-Redux</h1>
       
       <div className='linha'>
-        <Intervalo></Intervalo>
+        <Intervalo min={min} max={max}></Intervalo>
       </div>
 
       <div className='linha'>
-        <Media></Media>
-        <Soma></Soma>
-        <Sorteio></Sorteio>
+        <Media min={min} max={max}></Media>
+        <Soma min={min} max={max}></Soma>
+        <Sorteio min={min} max={max}></Sorteio>
       </div>
 
     </div>
--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -2,12 +2,13 @@ import React from "react"
 import Card from "./Card"
 
 export default props => {
+    const {min, max} = props
     return (
         <Card title="Soma dos Números" blue>
             <div>
                 <span>
-                    <span>Resultado:</span>
-                    <strong>10</strong>
+                    <span>Resultado: </span>
+                    <span>{min + max}</span>
                 </span>
             </div>
         </Card>
