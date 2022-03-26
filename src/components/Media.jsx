3b90ad1e6b65c676818c8392ffa944e34f3d4f0b
XandyGomes <x3.computadores@hotmail.com>
2022-03-26 12:45:46 -0300

    Aula

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -1,17 +1,25 @@
 import React from "react"
 import Card from "./Card"
 
-export default props => {
-    // const {min, max} = props
+function Media(props){
+    const {min, max} = props
     return (
         <Card title="Média dos Números" green>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    {/* <span>{(min + max)/2}</span> */}
-                    <span>{10}</span>
+                    <span>{(min + max)/2}</span>
                 </span>
             </div>
         </Card>
     )
 }
+
+function mapStateToProps(state) {
+    return {
+        min: state.numeros.min,
+        max: state.numeros.max
+    }
+}
+
+export default connect(mapStateToProps)(Media)
